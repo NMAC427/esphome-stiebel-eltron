ce7a5b52a17bbec67fb1657cd9e235019a4d9894
Nicolas Camenisch <garnele007@gmail.com>
2025-06-21 14:27:12 +0200

    Turn "WW Hysterese" into sensor, as it can't be set

--- components/stiebel_eltron_can/sensor.py
+++ components/stiebel_eltron_can/sensor.py
@@ -1,19 +1,21 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import CONF_UPDATE_INTERVAL, CONF_ACCURACY_DECIMALS
+from esphome.const import CONF_ACCURACY_DECIMALS, STATE_CLASS_MEASUREMENT
 from . import stiebel_eltron_can_ns, StiebelEltronCanComponent, ELSTER_TYPES, CAN_MEMBERS
 
 DEPENDENCIES = ['stiebel_eltron_can']
 StiebelEltronCanSensor = stiebel_eltron_can_ns.class_('StiebelEltronCanSensor', sensor.Sensor, cg.PollingComponent)
 
-CONFIG_SCHEMA = sensor.sensor_schema(StiebelEltronCanSensor).extend({
+CONFIG_SCHEMA = sensor.sensor_schema(
+    StiebelEltronCanSensor,
+    state_class=STATE_CLASS_MEASUREMENT
+).extend({
     cv.GenerateID('stiebel_eltron_can_id'): cv.use_id(StiebelEltronCanComponent),
     cv.Required('elster_index'): cv.hex_int,
     cv.Optional('elster_type', default='DEFAULT'): cv.enum(ELSTER_TYPES, upper=True, space="_"),
     cv.Optional('target'): cv.enum(CAN_MEMBERS, upper=True, space="_"),
-    cv.Optional(CONF_UPDATE_INTERVAL, default="30s"): cv.update_interval,
-}).extend(cv.COMPONENT_SCHEMA)
+}).extend(cv.polling_component_schema("60s")).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = await sensor.new_sensor(config)
